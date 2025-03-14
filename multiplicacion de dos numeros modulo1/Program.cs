53b3fe86b542eadd317b3e0cbd6e6d9e10df2ee1
juan manuel javier <lomanito@hotmail.com>
2025-03-14 19:21:10 -0400

    multiplicacion de dos numeros

---
+++ multiplicacion de dos numeros modulo1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace multiplicacion_de_dos_numeros_modulo1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                Console.Write("Ingrese el primer número: ");
+                double numero1 = Convert.ToDouble(Console.ReadLine());
+
+                Console.Write("Ingrese el segundo número: ");
+                double numero2 = Convert.ToDouble(Console.ReadLine());
+
+                double resultado = numero1 * numero2;
+
+                Console.WriteLine($"El resultado de la multiplicación es: {resultado}");
+            }
+        }
+    }
+    }
+
