6468effd1350560575fff53e9971c6059b12271f
juan manuel javier <lomanito@hotmail.com>
2025-03-14 19:43:25 -0400

    fibonacci

---
+++ serie fibonacci. modulo1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace serie_fibonacci.modulo1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Serie de Fibonacci hasta 50:");
+
+            int a = 1, b = 1;
+
+            Console.Write(a + " " + b + " ");
+
+            while (true)
+            {
+                int siguiente = a + b;
+                if (siguiente > 50) break;
+
+                Console.Write(siguiente + " ");
+                a = b;
+                b = siguiente;
+            }
+
+            Console.WriteLine(); // Salto de línea al final
+        }
+    }
+
+}
