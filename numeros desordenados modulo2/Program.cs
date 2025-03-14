7b85f0ebaea9cbd7aaeb15d3b679c87a93f66c53
juan manuel javier <lomanito@hotmail.com>
2025-03-14 19:51:30 -0400

    numeros desordenados

---
+++ numeros desordenados modulo2/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace numeros_desordenados_modulo2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                int[] numeros = new int[10];
+
+                // Ingresar los números
+                Console.WriteLine("Ingrese 10 números desordenados:");
+                for (int i = 0; i < 10; i++)
+                {
+                    Console.Write($"Número {i + 1}: ");
+                    numeros[i] = Convert.ToInt32(Console.ReadLine());
+                }
+
+                // Ordenar el arreglo
+                Array.Sort(numeros);
+
+                // Mostrar los números ordenados
+                Console.WriteLine("\nNúmeros ordenados:");
+                foreach (int num in numeros)
+                {
+                    Console.Write(num + " ");
+                }
+
+                Console.WriteLine(); // Salto de línea al final
+            }
+        }
+    }
+    }
+
