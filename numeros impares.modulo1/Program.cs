f9e28c22e00cb8d24162d50b94a734318b44c133
juan manuel javier <lomanito@hotmail.com>
2025-03-14 19:34:42 -0400

    numeros impares

---
+++ numeros impares.modulo1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace numeros_impares.modulo1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                Console.WriteLine("Números impares del 1 al 30:");
+
+                for (int i = 1; i <= 30; i += 2)
+                {
+                    Console.Write(i + " ");
+                }
+
+                Console.WriteLine(); // Salto de línea al final
+            }
+        }
+    }
+    }
+
