2163030c284863574e1b79e1b01c515d7bd933a0
juan manuel javier <lomanito@hotmail.com>
2025-03-14 20:01:18 -0400

    fecha de nacimiento

---
+++ Ejercicio propuesto 1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_propuesto_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                Console.Write("Ingrese su fecha de nacimiento (YYYY-MM-DD): ");
+                string input = Console.ReadLine();
+
+                if (DateTime.TryParse(input, out DateTime fechaNacimiento))
+                {
+                    Console.WriteLine($"Naciste un {fechaNacimiento.ToString("dddd")}.");
+                }
+                else
+                {
+                    Console.WriteLine("Fecha no válida. Asegúrate de ingresar el formato correcto (YYYY-MM-DD).");
+                }
+            }
+        }
+    }
+}
