6acc26102058acdcaa698ff643023b2fe4400392
juan manuel javier <lomanito@hotmail.com>
2025-03-21 12:14:08 -0400

    modulo 2 listo

---
+++ multiplicar. modulo 2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace multiplicar.modulo_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+                Console.Write("Ingrese el primer número: ");
+                double num1 = Convert.ToDouble(Console.ReadLine());
+
+                Console.Write("Ingrese el segundo número: ");
+                double num2 = Convert.ToDouble(Console.ReadLine());
+
+                double resultado = num1 * num2;
+
+                Console.WriteLine($"El resultado de la resta es: {resultado}");
+            }
+    }
+}
---
+++ resta modulo2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace resta_modulo2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Ingrese el primer número: ");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Ingrese el segundo número: ");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+
+            double resultado = num1 - num2;
+
+            Console.WriteLine($"El resultado de la resta es: {resultado}");
+        }
+    }
+}
